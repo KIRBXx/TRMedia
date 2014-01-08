4b8470af8e1cdcf75b7bb110ec1e172f95f572e7
KIRBXx <james@theracersmedia.com>
2014-01-08 17:42:37 -0500

    News Page Updates - Still not ready

--- app/views/news/articles/1.blade.php
+++ app/views/news/articles/1.blade.php
@@ -29,40 +29,7 @@
   <body>
 @include ('master/navbar')
 
-    <div class="container">
-
-      <div class="row row-offcanvas row-offcanvas-right">
-
-        <div class="col-xs-12 col-sm-9">
-          <div class="jumbotron">
-            <h1>TRM - News!</h1>
-            <p>This is our news section, where you can stay up to date on all things at The Racers Media!</p>
-          </div>
-          <div class="row">
-            <div class="col-6 col-sm-6 col-lg-8">
-              <h2>New Features + Changes</h2>
-              <p>The Developers at The Racers Media have been busy over the last few days, as you can see by the new features. Check out the list of new features by clicking "View More"</p>
-              <p><a class="btn btn-default" href="/news/1.blade.php" role="button">View More &raquo;</a></p>
-            </div><!--/span-->
-            <div class="col-md-4 hidden-xs">
-				<div class="panel panel-default panel-body">
-              <h2>Note</h2>
-              <p>The News section is updated at least once a week, depending on what is happening with Site Updates and what not.  </p>
-				</div>
-			</div><!--/span-->
-          </div><!--/row-->
-        </div><!--/span-->
-
-        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
-          <div class="list-group">
-            <a href="http://facebook.com/theracersmedia" class="list-group-item active">Live Updates via Facebook</a>
-            <a href="http://twitter.com/theracersmedia" class="list-group-item">Live Updates via Twitter</a>
-
-          </div>
-        </div><!--/span-->
-      </div><!--/row-->
-
-      <hr>
+ 
 
       <footer>
 @include ('master/footer')
