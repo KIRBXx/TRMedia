198aa0089440000797576dbc3bb137059ddafe43
KIRBXx <james@theracersmedia.com>
2014-01-09 03:41:37 -0500

    New Modals/Removed Useless files

---
+++ app/views/master/faq.blade.php
@@ -0,0 +1,21 @@
+								<!-- FAQ Modal -------------------------------------------------------------------------------->
+								  <div class="modal fade" id="faq" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+									<div class="modal-dialog">
+									  <div class="modal-content">
+										<div class="modal-header navbar-default">
+										  <button type="button" style="color: #fff" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+										  <h4 class="modal-title" style="color: #fff">FAQ</h4>
+										</div>
+										<div class="modal-body">
+										<p>Common Facts & Questions<p/>
+										<ul>
+										<li><strong>What type of images are allowed?</strong>	Any Motorsport Related Images are allowed (Even Video Games/Simulations Involving Motorsports!)</li>
+										</ul>
+										</div>
+										<div class="modal-footer">
+										  <a href="http://twitter.com/theracersmedia"><img src="/social/images/twitter.png"></a>										  
+										  <a href="http://facebook.com/theracersmedia"><img src="/social/images/facebook.png"></a>
+										</div>
+									  </div><!-- /.modal-content -->
+									</div><!-- /.modal-dialog -->
+								  </div><!-- FAQ/.modal ---------------------------------------------------------------------------->
---
+++ app/views/master/policy.blade.php
@@ -0,0 +1,18 @@
+								  <!-- POLICY Modal -------------------------------------------------------------------------------->
+								  <div class="modal fade" id="policy" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+									<div class="modal-dialog">
+									  <div class="modal-content">
+										<div class="modal-header navbar-default">
+										  <button type="button" style="color: #fff" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+										  <h4 class="modal-title" style="color: #fff">Privacy Policy</h4>
+										</div>
+										<div class="modal-body">
+										  <p>Privacy Policy Coming Soon!</p>
+										</div>
+										<div class="modal-footer">
+										  <a href="http://twitter.com/theracersmedia"><img src="/social/images/twitter.png"></a>										  
+										  <a href="http://facebook.com/theracersmedia"><img src="/social/images/facebook.png"></a>
+										</div>
+									  </div><!-- /.modal-content -->
+									</div><!-- /.modal-dialog -->
+								  </div><!-- POLICY/.modal ---------------------------------------------------------------------------->
